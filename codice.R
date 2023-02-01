d908e41310a7a69ee88aaa48d465a63f5d2f4be1
doinav <99347603+doinav@users.noreply.github.com>
2023-02-01 12:24:49 +0100

    Add files via upload

--- codice.R
+++ codice.R
@@ -1,3 +1,5 @@
-dataset = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/Spotify 2010 - 2019 Top 100.csv")
-
-data = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/song_data1.csv/song_data.csv")
+song_data = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/song_data1.csv/song_data.csv")
+na = is.na(song_data)
+library(dplyr)
+df = song_data %>% distinct()
+df2 <- df %>% distinct(song_name, .keep_all = TRUE)
