279c576a4c04d894fb7cebebb618a3e97c9a5e21
doinav <99347603+doinav@users.noreply.github.com>
2023-02-01 15:53:56 +0100

    Add files via upload

--- codice.R
+++ codice.R
@@ -1,5 +1,4 @@
 song_data = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/song_data1.csv/song_data.csv")
-na = is.na(song_data)
 library(dplyr)
 df = song_data %>% distinct()
 df2 <- df %>% distinct(song_name, .keep_all = TRUE)
@@ -9,11 +8,10 @@ df2['song_duration_ms'] = df2['song_duration_ms']/1000
 #metodo 1
 colnames(df2)[colnames(df2) == "song_duration_ms"] = "song_duration_s"
 #metodo2
-colnames(df2)[3] = "song_duration_s"
+#colnames(df2)[3] = "song_duration_s"
 #metodo3
-df3 <- df2 %>% rename(song_duration_s = song_duration_ms)
+#df3 <- df2 %>% rename(song_duration_s = song_duration_ms)
 
 
-write.table(df2, file = "data.csv",
-            sep = "\t", row.names = F)
-
+write.csv(df2, file = "data1.csv")
+summary(df2)
