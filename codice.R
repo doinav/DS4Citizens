2d8bac164d8361c5373b58f71d7dad8527ad60f0
doinav <99347603+doinav@users.noreply.github.com>
2023-02-01 10:28:21 +0100

    Add files via upload

---
+++ codice.R
@@ -0,0 +1,3 @@
+dataset = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/Spotify 2010 - 2019 Top 100.csv")
+
+data = read.csv("/Users/anato/Desktop/Università/Data Science 4 Citiziens/song_data1.csv/song_data.csv")
