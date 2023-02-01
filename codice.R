16ba55ad3f3372151396624fe79afcc127022bd3
doinav <99347603+doinav@users.noreply.github.com>
2023-02-01 13:17:27 +0100

    Add files via upload

--- codice.R
+++ codice.R
@@ -3,3 +3,17 @@ na = is.na(song_data)
 library(dplyr)
 df = song_data %>% distinct()
 df2 <- df %>% distinct(song_name, .keep_all = TRUE)
+df2['song_duration_ms'] = df2['song_duration_ms']/1000
+
+#sostituire i nomi
+#metodo 1
+colnames(df2)[colnames(df2) == "song_duration_ms"] = "song_duration_s"
+#metodo2
+colnames(df2)[3] = "song_duration_s"
+#metodo3
+df3 <- df2 %>% rename(song_duration_s = song_duration_ms)
+
+
+write.table(df2, file = "data.csv",
+            sep = "\t", row.names = F)
+
